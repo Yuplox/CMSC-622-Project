c42559804b94275d9b19a299a4d676150dd5d910
Zachary James <znjames123@gmail.com>
2026-04-04 13:18:49 -0400

    Fixed use of && instead of and

--- run.py
+++ run.py
@@ -11,15 +11,28 @@ def run(useCase):
     net = Mininet(topo=SatelliteTopo(10, 100, '250ms', 1), link=TCLink)
     net.start()
 
+    # Get all hosts in topology
+    server = net.get('ser0')
+    termA = net.get('term0')
+    termB = net.get('term1')
+
+
     if (useCase == '3.1'):
-        # Simulate use case 3.1;
+        # Start use case 3.1 scripts in the background
+        server.cmd('python3 server31.py &')
+        termA.cmd('python3 terminal31.py &')
+        termB.cmd('python3 terminal31.py &')
     elif (useCase == '3.2'):
         # Simulate use case 3.2
 
+    # Start mininet command line
+    CLI(net)
+
+    # Stop mininet after exiting command line
     net.stop()
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2 && (sys.argv[1] == '3.1' || sys.argv[1] == '3.2'):
+    if len(sys.argv) == 2 and (sys.argv[1] == '3.1' || sys.argv[1] == '3.2'):
         run(sys.argv[1])
     else:
         print('Expected usage: run.py [USE_CASE]')
